d854211bb71f58a4d892fb10021152075033cbe3
zoujylive2017 <505604564@qq.com>
2017-07-18 17:18:32 +0800

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,22 +1,29 @@
-//js中的三大排序方法
-
-<script>
-  //1，冒泡排序
+//三大排序算法: 冒泡排序
+  
   function bubbleSort(arr){
-    for(var l=1;l<arr.length;l++){
-      for(var i=0;i<arr.length-l;i++){
+    //外层循环控制轮数
+    for(var r=1;r<arr.length;r++){
+      for(var i=0;i<arr.length-r;i++){
+        //如果i位置的值>i+1位置的值
         if(arr[i]>arr[i+1]){
-          arr[i+1]=[arr[i],arr[i]=arr[i+1]][0];
+          //交换两位置的值
+//          arr[i]^=arr[i+1];
+//          arr[i+1]^=arr[i];
+//          arr[i]^=arr[i+1];
+          //问题：只支持number类型
+          //解决: b=[a,a=b][0];
+          arr[i+1]=[
+            arr[i],
+            arr[i]=arr[i+1]
+          ][0];
         }
       }
     }
   }
-  var arr=[1,5,8,44,2,11,478];
+  var arr=//[4,6,3,5,2,1];
+          //[12,2,123,3,23,1];
+          ["rose","jack","tom","jerry"];
   bubbleSort(arr);
   console.log(String(arr));
 
 
-
-
-
-</script>
